713a700f0e10f5f6dfc711c9dcb20caaa0925815
Allan MacLean <33722929+yellowzoneallan@users.noreply.github.com>
2020-06-25 15:24:42 +0100

    extra files
    ssh keys - best to use your own
    picluster.sh - example for accessing all your pis
    Vagrant file - build ubuntu 20.04 2Gb VM, install k3s (Kubernetes) and install various micro services - google shop, Prometheus &Grafana

---
+++ k8s-on-pi-vagrant/picluster.sh
@@ -0,0 +1,10 @@
+ssh -i allan-pi-key ubuntu@192.168.1.201 $1
+ssh -i allan-pi-key ubuntu@192.168.1.202 $1
+ssh -i allan-pi-key ubuntu@192.168.1.203 $1
+ssh -i allan-pi-key ubuntu@192.168.1.204 $1
+ssh -i allan-pi-key ubuntu@192.168.1.205 $1
+ssh -i allan-pi-key ubuntu@192.168.1.206 $1
+ssh -i allan-pi-key ubuntu@192.168.1.207 $1
+ssh -i allan-pi-key ubuntu@192.168.1.208 $1
+ssh -i allan-pi-key ubuntu@192.168.1.209 $1
+ssh -i allan-pi-key ubuntu@192.168.1.210 $1
